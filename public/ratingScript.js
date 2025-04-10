c71cd8ca7b29379f732c428d6c30a31e29e36768
akdoingse12 <akdoingse12@gmail.com>
2025-04-10 18:06:11 +0000

    sjbsjdjs

--- public/ratingScript.js
+++ public/ratingScript.js
@@ -1,11 +1,11 @@
 // Add an event listener to the form that upon submits creates a new rating
 // When the rating is created redirect to the ratings page
 const ratingform = document.querySelector("ratingform")
-rating.adddEventListener("submit", async(e)=>{
+ratingform.adddEventListener("submit", async(e)=>{
     e.preventDefault();
 
     const formData = new FormData(ratingForm);
-    const ratingData = Object.formEntries(formData.entries(ratingData));
+    const ratingData = Object.formEntries(ratingData);
 
 const response = await fetch("/add/rating", {
     method: "Post",
--- public/teacherScript.js
+++ public/teacherScript.js
@@ -1,11 +1,11 @@
 // Add an event listener to the form that upon submits creates a new teacher
 // When the teacher is created redirect to the teachers page
 const teacherform = document.querySelector("teacherform")
-teacher.adddEventListener("submit", async(e)=>{
+teacherform.addEventListener("submit", async(e)=>{
     e.preventDefault();
 
     const formData = new FormData(teacherForm);
-    const teacherData = Object.formEntries(formData.entries(formData));
+    const teacherData = Object.formEntries(formData);
 
 const response = await fetch("/add/teachers", {
     method: "Post",
